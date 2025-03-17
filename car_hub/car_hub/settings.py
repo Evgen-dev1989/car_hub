c8615237adb087bb3aec886476e75dc57cd79dfb
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-03-17 18:16:30 +0200

    +html

--- car_hub/car_hub/settings.py
+++ car_hub/car_hub/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'car_hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
