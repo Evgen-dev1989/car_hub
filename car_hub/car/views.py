c6015be97784f9732d2581e67bfd17fe088bc69b
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-03-17 22:15:51 +0200

    +

--- car_hub/car/views.py
+++ car_hub/car/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from car_hub.services import  get_categories
 
 def cars_page(request):
-    return render(request, 'cars.html')
+    return render(request, 'cars.html', {'categories':get_categories(request)})
--- car_hub/car_hub/services.py
+++ car_hub/car_hub/services.py
@@ -1,15 +1,14 @@
 from car .models import Category
 
-passenger_car = Category.objects.create(name = 'passenger car')
-cargo_car = Category.objects.create(name = 'cargo car')
+# passenger_car = Category.objects.create(name = 'passenger car')
+# cargo_car = Category.objects.create(name = 'cargo car')
 
-sedan = Category.objects.create(name="Седаны", parent=passenger_car)
-crossover = Category.objects.create(name="Кроссоверы", parent=passenger_car)
-fura = Category.objects.create(name="Фуры", parent=cargo_car)
-pickup = Category.objects.create(name="Пикапы", parent=cargo_car)
+# sedan = Category.objects.create(name="Седаны", parent=passenger_car)
+# crossover = Category.objects.create(name="Кроссоверы", parent=passenger_car)
+# fura = Category.objects.create(name="Фуры", parent=cargo_car)
+# pickup = Category.objects.create(name="Пикапы", parent=cargo_car)
 
 def get_categories():
-    return Category.objects.filter(
-        passenger_car=user,
-     
-    )
+    return Category.objects.all()
+
+print(get_categories())
