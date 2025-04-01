87011573e264ff340061bdc98c2eefa2f746422b
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-04-01 23:48:46 +0300

    +html

--- car_hub/car/models.py
+++ car_hub/car/models.py
@@ -26,7 +26,7 @@ class Car(models.Model):
     year = models.PositiveIntegerField(default=2000, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=True, related_name="cars")  
-
+    reviews = models.CharField(max_length=100, null=True, blank=True)
     def __str__(self):
         return f"{self.brand} {self.model} ({self.year})"
 
