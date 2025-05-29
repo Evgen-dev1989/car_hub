b27ca6c05db12f19a68704d61205ac4a6c80602a
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-05-29 18:33:35 +0300

    +
    LOGIN_REDIRECT_URL

--- car_hub/car_hub/settings.py
+++ car_hub/car_hub/settings.py
@@ -43,6 +43,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = password  
 EMAIL_HOST_USER = email_host_user 
 
+LOGIN_REDIRECT_URL = '/car/'
+LOGOUT_REDIRECT_URL = '/car/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
