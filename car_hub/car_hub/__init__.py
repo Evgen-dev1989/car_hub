f34f6e993da6e03c3a1b32f28f804f20c4a31102
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-06-03 18:32:30 +0300

    + celery, redis

---
+++ car_hub/car_hub/__init__.py
@@ -0,0 +1,3 @@
+from car_hub.celery import app as celery_app
+
+__all__ = ('celery_app',)
--- car_hub/car_hub/settings.py
+++ car_hub/car_hub/settings.py
@@ -17,6 +17,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.github.GithubOAuth2',
     'social_core.backends.facebook.FacebookOAuth2',
